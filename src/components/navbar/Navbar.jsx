026bcbacec6e1401d3fd5650f599012c1d1a3915
nidhinagpure <nagpurenidhi8@gmail.com>
2025-02-28 17:18:19 +0530

    Add choose use

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,6 +8,7 @@ import Farmer from './../navbar/farm.png';
 import Earth from './../navbar/earth.0b9cdbcbe4284655c245.png';
 import Trak from './../navbar/tract.3159894a3d7326a93f87.png';
 import Vegetables from './../navbar/vegetable.164d2731f5f12bc80d38.png';
+import Choose  from './../navbar/leaf.png';
 
 export default function Navbar() {
   return (
@@ -32,38 +33,44 @@ export default function Navbar() {
         <div className='hero-Content'>
           <span className='hero-paragraph'>Fresh agri products bring the goodness of nature straight to your table, ensuring quality, nutrition, and flavor in every bite .</span><br></br>
           <span className='hero-Paragraph'>Our website connects you directly with local farmers, offering a range of farm-fresh organic products delivered straight to your door.We ensure quality, sustainability, and fair pricing.</span>
-       
 
-       
+
+
           <div className='section-Icons'>
-          <img src={Earth} className='hero-Icons'></img>
-          <img src={Cow} className='hero-Icons'></img>
-          <img src={Vegetables} className='hero-Icons'></img>
-          <img src={Farmer} className='hero-Icons'></img>
-          <img src={Flower} className='hero-Icons'></img>
-          <img src={Trak} className='hero-Icons'></img>
+            <img src={Earth} className='hero-Icons'></img>
+            <img src={Cow} className='hero-Icons'></img>
+            <img src={Vegetables} className='hero-Icons'></img>
+            <img src={Farmer} className='hero-Icons'></img>
+            <img src={Flower} className='hero-Icons'></img>
+            <img src={Trak} className='hero-Icons'></img>
           </div>
 
           <div className='section-Texts'>
             <p className='section-Text'>Organic<br></br>
-            Farming</p>
+              Farming</p>
             <p className='section-Text'>Diffrent<br></br>
-            Livestock</p>
+              Livestock</p>
             <p className='section-Text'>Fresh<br></br>
-            Vegetables</p>
+              Vegetables</p>
             <p className='section-Text'>Farm<br></br>
-            Fresh</p>
+              Fresh</p>
             <p className='section-Text'>Beautiful<br></br>
-            Flowers</p>
+              Flowers</p>
             <p className='section-Text'>Modern<br></br>
-            Equipment</p>
+              Equipment</p>
           </div>
-          </div>
-
-       
-
+        </div>
       </div>
-
+           <div className='choose-Section'>
+            <img src={Choose} className='choose-Icons'></img>
+            <div className=''>
+              <span className='choose'>Why  Choose Us ? </span>
+              <span className='choose-Text'>By building a culture of sustainability and wholesome living, we strive for a <br></br>
+                healthier you and a healthier planet.</span><br></br>
+              <button type='button' className='choose-Btn'>Know more</button>
+            </div>
+            <img src={Choose} className='choose-Icons'></img>
+           </div>
 
 
     </div>
