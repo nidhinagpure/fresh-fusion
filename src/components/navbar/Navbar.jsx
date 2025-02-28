a086b281779262dae29daf4fc18094922d815dd2
nidhinagpure <nagpurenidhi8@gmail.com>
2025-02-28 13:34:41 +0530

    Add Hero Section

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -17,6 +17,21 @@ export default function Navbar(){
             </div>
             <img src={Image} className='main-Image'></img>
 
+               <div className='hero-Container'>
+
+                <div className='hero-Text'>
+                  <span className='hero-head'>Agrimart <br></br>Farm Fresh <br></br> Products</span> 
+                </div>
+
+                <div className='hero-Content'>
+                <span className='hero-paragraph'>Fresh agri products bring the goodness of nature straight to your table, ensuring quality, nutrition, and<br></br> flavor in every bite</span> <br></br>
+                <span className='hero-paragraph'>Our website connects you directly with local farmers, offering a range of farm-fresh organic products delivered straight to your door. We ensure quality, sustainability, and fair pricing.</span>
+                </div>
+
+                </div>
+            
+               
+
         </div>
     )
 }
